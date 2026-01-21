844940bc59c2ef21284d5ce6b865cf19c073f91e
osama-786 <osamafarooqui786@gmail.com>
2026-01-21 17:57:44 +0530

    Added Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from 'react-dom/client'
+import ReactGA from "react-ga4";
 import App from './App.tsx'
 import './index.css'
+
+// <--- 2. Initialize GA4 with your ID
+ReactGA.initialize("G-QT4EL430C4");
+
+// <--- 3. Send the first pageview event
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname, 
+  title: "Home Page"
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
