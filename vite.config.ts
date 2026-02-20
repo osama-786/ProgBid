f7baa067bd83a631b81214e4ab072d0143339498
osama-786 <osamafarooqui786@gmail.com>
2026-02-20 16:22:17 +0530

    Instal React snap

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { vitePrerenderPlugin } from "vite-prerender-plugin";
 
 export default defineConfig(({ mode }) => ({
   server: { host: "::", port: 8080 },
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
-    vitePrerenderPlugin({
-      renderTarget: '#root', 
-      prerenderScript: path.resolve(__dirname, './prerender.tsx'),
-    }),
   ].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   build: {
